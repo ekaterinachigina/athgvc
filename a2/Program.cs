5ab1ba442a9d307804b694c28cd8ee1adcd3c14b
Katya <katyalolka1994@gmail.com>
2022-09-18 08:43:21 +0300

    first commit

---
+++ a2/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] GatArray(int size, int minValue, int maxValue)
+{
+    Console.Write("[");
+    int[] res = new int [size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+        Console.Write($"{res[i]} ");
+    }
+    Console.Write("]");
+    return res;
+}
+int [] array = GatArray(7, -100, 100);
+int SumNumbers(int[] array1)
+{
+    int index = 0;
+    int n = 0;
+    int sum = 0;
+
+    while (index < array1.Length)
+    {
+        if (n % 2 != 0)
+        {
+            sum = sum + array1[n];
+        }
+            index = index + 1;
+            n = n + 1;
+        
+    }
+    return sum;
+}
+Console.WriteLine();
+Console.WriteLine($"Сумма элементов, стоящих на нечетных позициях массива = {SumNumbers(array)}");
---
+++ a3/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+int[] GatArray (int size, int minValue, int maxValue)
+{
+    Console.Write("[");
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+        Console.Write($"{res[i]}");
+    }
+    Console.Write("]");
+    return res;
+}
+
+int[] array = GatArray(5, 1, 10);
+int MaxNumbers(int[] maxnumber)
+{
+    int index = 1;
+    int max = maxnumber[0];
+    int n = 1;
+    while (index < maxnumber.Length)
+    {
+        if (maxnumber[n] > max)
+        {
+            max = maxnumber[n];
+        }
+        index = index + 1;
+        n = n + 1;
+    }
+    return max;
+}
+int MinNumbers(int[] minnumber)
+{
+    int index = 1;
+    int min = minnumber[0];
+    int n = 1;
+    while (index < minnumber.Length)
+    {
+        if (minnumber[n] < min)
+        {
+            min = minnumber[n];
+        }
+        index = index + 1;
+        n = n + 1;
+    }
+    return min;
+}
+Console.WriteLine();
+int difference = MaxNumbers(array) - MinNumbers(array);
+Console.WriteLine($"Разница между максиманым и минимальным значением массива = {difference}");
